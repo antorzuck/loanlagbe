5b4f9c4190efb0c18631e97d768b64fc0de86dc9
antor <antorzuck>
2024-06-27 13:01:40 +0600

    docsaded

--- base/models.py
+++ base/models.py
@@ -41,10 +41,17 @@ class Customer(Base):
 
 class Document(Base):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    file_type = models.CharField(max_length=100)
-    file = models.FileField(upload_to='customer-documents')
 
+    cus_nid = models.FileField(null=True, blank=True,upload_to='customer-documents')
+    cus_photo = models.FileField(null=True, blank=True,upload_to='customer-documents')
+    cus_stamp = models.FileField(null=True, blank=True,upload_to='customer-documents')
 
+    jamin_nid = models.FileField(null=True, blank=True,upload_to='customer-documents')
+    jamin_photo = models.FileField(null=True, blank=True,upload_to='customer-documents')
+    jamin_stamp = models.FileField(null=True, blank=True,upload_to='customer-documents')
+
+    def __str__(self):
+        return f"documents of {self.customer.namr}"
 
 
 class Payment(Base):
